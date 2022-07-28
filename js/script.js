32456644cd7bd58627f7bd23ce7e0fa9128ac911
Nadezhda <oleneva96@gmail.com>
2022-07-28 15:20:49 +0300

    added close modal js

--- js/script.js
+++ js/script.js
@@ -14,4 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     })
 
+    const sign_in_btn = document.querySelector('.sign_in_btn');
+    const close_btn = document.querySelector('.close_btn');
+    const box_form = document.querySelector('.box_form');
+    const container_modal = document.querySelector('.container_modal')
+
+    sign_in_btn.addEventListener('click', () => {
+        container_modal.classList.remove('hide');
+    })
+
+    close_btn.addEventListener('click', () => {
+        container_modal.classList.add('hide');
+    })
+
+    container_modal.addEventListener('click', () => {
+        container_modal.classList.add('hide');
+    })
+
+    box_form.addEventListener('click', (e) => {
+        e.stopPropagation();
+    })
+
+
 });
